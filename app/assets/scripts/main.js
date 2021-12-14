d9896c24c2666caf583f901197eee55707d0e342
joe.chapman <contemno82@gmail.com>
2021-12-14 13:56:17 +0000

    hardcoded link - oops

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,5 +1,5 @@
 if(document.getElementById('qrcode')) {
-    new QRCode(document.getElementById("qrcode"), "http://localhost:8900/start?user=f153ab26-2767-4607-bbd3-4fadaded5882");
+    new QRCode(document.getElementById("qrcode"), "/start?user=f153ab26-2767-4607-bbd3-4fadaded5882");
     const title = document.getElementById('qrcode').title;
     console.log(title);
     document.getElementById('qrlink').href = title;
